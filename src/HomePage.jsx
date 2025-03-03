c4dec7f577947ccf6295f954b05a468e6c445be6
zmktggroup <zmktggroup@gmail.com>
2025-03-03 14:35:59 -0500

    Fixed homepage and got website running

---
+++ src/HomePage.jsx
@@ -0,0 +1,13 @@
+// src/HomePage.jsx
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Z Marketing Group</h1>
+      <p>This is the homepage of your site.</p>
+    </div>
+  );
+}
+
+export default HomePage;
